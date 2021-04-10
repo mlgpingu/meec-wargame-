b08b803a6f3df9ea5def8bfe16d855663a5aef72
mlgpingu <82107690+mlgpingu@users.noreply.github.com>
2021-04-10 19:20:08 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -7,11 +7,13 @@ int i, j;              //Variáveis a usar nos for loops
 
 void imprimirTabuleiro() {                //Funcão para apresentar o tabuleiro de jogo
     for(i=1; i<=9; i++) {
+        printf(" %d ", i);
         for(j=1; j<=15; j++) {
-            printf(" %c ", tabuleiro[i][j]);
+            printf(" %c  ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+    printf("    01  02  03  04  05  06  07  08  09  10  11  12  13  14  15");
 }
 
 int invalidez = 0;     //Variável que indica se a jogada é válida
