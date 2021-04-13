b5f4d7b3449581683f10f6da9845b62389415048
mlgpingu <82107690+mlgpingu@users.noreply.github.com>
2021-04-13 16:47:33 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#define altura 9
-#define largura 15
+#define altura 8
+#define largura 14
 
 char tabuleiro[altura][largura];  //Declaração do tabuleiro de jogo
 
 int i, j;              //Variáveis a usar nos for loops
 
 void imprimirTabuleiro() {                //Funcão para apresentar o tabuleiro de jogo
-    for(i=1; i<=9; i++) {
-        printf(" %d ", 10-i);
-        for(j=1; j<=15; j++) {
+    for(i=0; i<=altura; i++) {
+        printf(" %d ", 9-i);
+        for(j=0; j<=largura; j++) {
             printf(" %c ", tabuleiro[i][j]);
         }
         printf("\n");
@@ -20,8 +20,8 @@ void imprimirTabuleiro() {                //Funcão para apresentar o tabuleiro 
 int invalidez = 0;     //Variável que indica se a jogada é válida
 
 void testeValidez1() {                   //Função para testar se a jogada é válida de acordo com a restrição 1
-    for(i=1; i<=8; i++) {
-        for(j=1; j<=14; j++) {
+    for(i=0; i<altura; i++) {
+        for(j=0; j<largura; j++) {
             if((tabuleiro[i][j] != '-' && tabuleiro[i+1][j] != '-') || (tabuleiro[i][j] != '-' && tabuleiro[i][j+1] != '-')
                || (tabuleiro[i][j] != '-' && tabuleiro[i+1][j+1] != '-'))
                 invalidez = 1;
@@ -41,8 +41,8 @@ int main() {
     printf("MEEC War Game\n\nPrima qualquer tecla para iniciar\n\n");
     getchar();
 
-    for(i=1; i<=9; i++) {     //Inicialização do tabuleiro de jogo
-        for(j=1; j<=15; j++) {
+    for(i=0; i<=altura; i++) {     //Inicialização do tabuleiro de jogo
+        for(j=0; j<=largura; j++) {
             tabuleiro[i][j] = '-';
         }
     }
